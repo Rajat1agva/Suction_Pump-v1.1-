d099eee2c76f5c45e14baf3480afbf0f786abc28
RAJAT MISHRA <rajatmishra@agvahealthcare.com>
2021-10-13 14:53:16 +0530

    digit update

--- Suction_PUMP(v1.1)/suction_pump/main.cpp
+++ Suction_PUMP(v1.1)/suction_pump/main.cpp
@@ -7,6 +7,7 @@
 
 
 /*************Dependencies*************/
+
 #include <Arduino.h>
 #include "AMS5812.h"
 #include "SPI.h"
@@ -15,7 +16,9 @@
 #include "FreeSerif18pt7b.h"
 #include "FreeSerif12pt7b.h"
 #include "icon.h"
+
 /************************************/
+
 #define TFT_RST 8   //Reset 
 #define TFT_RS  9   //Register select
 #define TFT_CS  10  // SS
@@ -24,6 +27,7 @@
 #define TFT_LED 3   // 0 if wired to +5V directly
 
 /*************************************/
+
 #define TFT_BRIGHTNESS 255  //Set tft brightness
 #define WHITE COLOR_WHITE
 #define BLACK COLOR_BLACK
@@ -34,6 +38,7 @@
 #define secondry_color COLOR_BLACK
 
 /*****************Variables*************************/
+
 float pressure = 0;
 float Previous_Pressure = 0;
 float Current_Pressure = 0;
@@ -42,79 +47,43 @@ int Current_Area = 0;
 int w = 10;
 int h = 15;
 String stringTwo;
-/******************************************/
+char first_digit = 0;
+char second_digit = 0;
+char third_digit = 0;
 
+/************************************************/
 
-/***************************************************************************************/
 TFT_22_ILI9225 tft = TFT_22_ILI9225(TFT_RST, TFT_RS, TFT_CS, TFT_LED, TFT_BRIGHTNESS);
 AMS5812 dPress(Wire,0x10,AMS5812::AMS5812_1000_D);
 void fill_area(int area,uint16_t colour);
-void AgVa_logo(void);
-/**************************************************************************************/
+void draw_scale();
+void AgVa_logo();
+void main_screen();
+
+/***************************************************************************************/
+
 void setup() {
   Serial.begin(9600);
   tft.begin();  //Tft initialize 
   tft.setOrientation(3);  //Set tft Orientation
   tft.setBacklight(HIGH); //Set back light high
   tft.setFont(Terminal6x8);
-  String s2;
   //Printing AgVa logo on screen
-  tft.fillRectangle(0,0,219,176,COLOR_WHITE);
-  tft.setGFXFont(&FreeSerif12pt7b);    //Set gfx font free serif
-  s2="AgVa";
-  tft.drawGFXText(20,90, s2,COLOR_DEEP_PINK);
-  s2="Healthcare";
-  tft.drawGFXText(90,90, s2,COLOR_BLACK);
-  delay(5000);
-  tft.clear();
-   tft.drawRectangle(0, 0, tft.maxX() - 1, tft.maxY() - 1, primary_color);  //Drawing rectangle at boundary of tft screen.
-  tft.drawRectangle(1, 1, tft.maxX() - 2, tft.maxY() - 2, primary_color);
-  tft.drawRectangle(2, 2, tft.maxX() - 3, tft.maxY() - 3, primary_color);
-  
-  tft.drawRectangle(30, 100, 190, 140,primary_color);  //Draw rectangle to show pressure display
-  tft.drawRectangle(28, 98, 192, 142, primary_color);
-  tft.drawRectangle(27, 97, 193, 143, primary_color);
-  //Scale
-  tft.drawLine(30,140,30,155,primary_color);
-  tft.drawText(27,158,"0",primary_color);
-  tft.drawLine(50,140,50,150,primary_color);
-  tft.drawLine(70,140,70,150,primary_color);
-  tft.drawLine(90,140,90,150,primary_color);
-  
-  tft.drawLine(110,140,110,155,primary_color);
-   tft.drawText(97,158,"0.05",primary_color);
-  tft.drawLine(130,140,130,150,primary_color);
-  tft.drawLine(150,140,150,150,primary_color);
-  tft.drawLine(170,140,170,150,primary_color);
-  tft.drawLine(190,140,190,155,primary_color);
-  tft.drawText(180,156,"0.1",primary_color);
-  
-  
-  tft.setGFXFont(&FreeSerif18pt7b);
-  
-  s2="0.000";
-  tft.drawGFXText(30,70, s2,primary_color); //Initially  show pressure zero
-  tft.setGFXFont(&FreeSerif12pt7b);
-  s2="MPa";
-  tft.drawGFXText(130,65, s2,primary_color); //Draw MPa text on screen
-  tft.setGFXFont(&FreeSerif18pt7b); 
-  tft.drawBitmap(170, 10, batt3, 30, 20, primary_color); //Battery icon
- 
+  AgVa_logo();
+  main_screen();
   while(!Serial){}
-  // starting communication with the 
-  // static pressure transducer
-  if (dPress.begin() < 0) {
+  if (dPress.begin() < 0) 
+  {
     Serial.println("Error communicating with sensor, check wiring and I2C address");
     while(1){}
   }
 }
 
-
+/*********************************************************************************/
 void loop() {
   
- 
   float final_pressure = 0;
-  int i = 200;
+  uint8_t i = 200;
   
  //Taking 2oo pressure value samples 
   while(i)
@@ -131,74 +100,157 @@ void loop() {
     delay(1);
   }
   final_pressure = final_pressure/200;
-
-  
   Serial.print("Pressure =\t");
   Serial.print(final_pressure,4);
   Serial.print("kPa");
   //Serial.print(Angle);
   Serial.print("\n");
-
-  Current_Area = map(final_pressure,0,100,0,4500);
-  
+  Current_Area = map(final_pressure,0,100,0,4500); //Maping between Pressure and Area
   Current_Pressure = final_pressure; 
-  stringTwo =  String(Current_Pressure/1000, 3); 
+  stringTwo =  String(Current_Pressure/1000, 3);
+     
+	 if((Current_Pressure-Previous_Pressure> 1)||(Previous_Pressure-Current_Pressure > 1))   //Printing Pressure Value Screen
+  { 
+	  if(third_digit!=stringTwo[4])
+	  {tft.fillRectangle(94, 45, 113, 75,COLOR_BLACK); // Rectangle  to delete Third digit	  
+	   third_digit = stringTwo[4];
+	  }
+	  if(second_digit != stringTwo[3])
+	  {
+		  tft.fillRectangle(76, 45, 94, 75, COLOR_BLACK); // Rectangle  to delete second digit
+		  second_digit = stringTwo[3];
+	  }
+	  if(first_digit != stringTwo[2])
+	  {
+		tft.fillRectangle(60, 45, 75, 75, COLOR_BLACK); // Rectangle  to delete first digit
+		first_digit = stringTwo[2];   
+	  }
+        tft.drawGFXText(30,70, stringTwo,primary_color);  //Writing new value from screen
+      
+   }
+   
+   
+      if((Current_Pressure-Previous_Pressure > 1))   //When Pressure is increasing.
+   {
   
-  if((Current_Pressure-Previous_Pressure > 1)||(Previous_Pressure-Current_Pressure > 1))
-  {
-  tft.fillRectangle(30, 40, 120, 80, COLOR_BLACK); //Removing previous pressure valve from screen
-  
-  tft.drawGFXText(30,70, stringTwo,primary_color);  //Writing new value from screen
+      if(Current_Area >= 0 && Current_Area < 900)
+       {
+		   fill_area(Current_Area,COLOR_GOLD);
+        }
+      else if(Current_Area >=900 && Current_Area <3600)
+      {
+          fill_area(Current_Area,COLOR_GREEN);
+      }
+      else if(Current_Area >= 3600 )
+      {
+       fill_area(Current_Area,COLOR_RED); 
+      }
+      
+	  Previous_Pressure = Current_Pressure;
+      
+	  Previous_Area = Current_Area;
+	  
   }
-  if((Current_Pressure-Previous_Pressure > 1))   //When Pressure is increasing.
- {
-  
-  if(Current_Area >= 0 && Current_Area < 900)
-  {fill_area(Current_Area,COLOR_GOLD);
-  }
-  else if(Current_Area >=900 && Current_Area <3600)
-  {
-    fill_area(Current_Area,COLOR_GREEN);
-  }
-  else if(Current_Area >= 3600 )
-  {
-   fill_area(Current_Area,COLOR_RED); 
-  }
-  Previous_Pressure = Current_Pressure;
-  Previous_Area = Current_Area;
-}
 
-else if(Previous_Pressure-Current_Pressure > 1)  //Pressure is decreasing.
+    else if(Previous_Pressure-Current_Pressure > 1)  //Pressure is decreasing.
 {
- float Previous_length = Previous_Area/rectangle_width;
- float Current_length =  Current_Area/rectangle_width;
- tft.fillRectangle(35+Current_length, 105, 35+Previous_length, 135,COLOR_BLACK);
-  if (Current_Area<50)
+   float Previous_length = Previous_Area/rectangle_width;
+  
+   float Current_length =  Current_Area/rectangle_width;
+  
+   tft.fillRectangle(35+Current_length, 105, 35+Previous_length, 135,COLOR_BLACK);
+  
+   if (Current_Area<50)
   {
-	fill_area(Current_Area,COLOR_BLACK); 
+	 fill_area(Current_Area,COLOR_BLACK); 
   }
+  
   else if(Current_Area >= 50 && Current_Area < 900)
   {
     fill_area(Current_Area,COLOR_GOLD);
   }
+  
   else if(Current_Area >=900 && Current_Area <3600)
   {
     fill_area(Current_Area,COLOR_GREEN);
   }
+  
   else if(Current_Area >= 3600 )
   {
    fill_area(Current_Area,COLOR_RED); 
   }
+  
   Previous_Pressure = Current_Pressure;
+  
   Previous_Area = Current_Area;
+
+ }
 }
-}
+
+/*******************************************************************************/
 
 void fill_area(int area,uint16_t colour)
 {
   
   int length = area/rectangle_width;
+  
   tft.fillRectangle(35, 105, 35+length, 135, colour);
   
 }
 
+
+/******************************************************************************/
+void draw_scale()
+{
+	 tft.drawLine(30,140,30,155,primary_color);
+	 tft.drawText(27,158,"0",primary_color);
+	 tft.drawLine(50,140,50,150,primary_color);
+	 tft.drawLine(70,140,70,150,primary_color);
+	 tft.drawLine(90,140,90,150,primary_color);
+	 
+	 tft.drawLine(110,140,110,155,primary_color);
+	 tft.drawText(97,158,"0.05",primary_color);
+	 tft.drawLine(130,140,130,150,primary_color);
+	 tft.drawLine(150,140,150,150,primary_color);
+	 tft.drawLine(170,140,170,150,primary_color);
+	 tft.drawLine(190,140,190,155,primary_color);
+	 tft.drawText(180,156,"0.1",primary_color);
+}
+/***************************************************************************/
+
+void AgVa_logo()
+{    String s2;
+	 tft.fillRectangle(0,0,219,176,COLOR_WHITE);
+	 tft.setGFXFont(&FreeSerif12pt7b);    //Set gfx font free serif
+	 s2="AgVa";
+	 tft.drawGFXText(20,90, s2,COLOR_DEEP_PINK);
+	 s2="Healthcare";
+	 tft.drawGFXText(90,90, s2,COLOR_BLACK);
+	 delay(1000);
+	 tft.clear();
+}
+
+/************************************************************************/
+
+void main_screen()
+{
+tft.drawRectangle(0, 0, tft.maxX() - 1, tft.maxY() - 1, primary_color);  //Drawing rectangle at boundary of tft screen.
+tft.drawRectangle(1, 1, tft.maxX() - 2, tft.maxY() - 2, primary_color);
+tft.drawRectangle(2, 2, tft.maxX() - 3, tft.maxY() - 3, primary_color);
+
+tft.drawRectangle(30, 100, 190, 140,primary_color);  //Draw rectangle to show pressure display
+tft.drawRectangle(28, 98, 192, 142, primary_color);
+tft.drawRectangle(27, 97, 193, 143, primary_color);	
+ tft.setGFXFont(&FreeSerif18pt7b);
+ 
+ String s2="0.000";
+ tft.drawGFXText(30,70, s2,primary_color); //Initially  show pressure zero
+ tft.setGFXFont(&FreeSerif12pt7b);
+ s2="MPa";
+ tft.drawGFXText(130,65, s2,primary_color); //Draw MPa text on screen
+ tft.setGFXFont(&FreeSerif18pt7b);
+ //tft.drawBitmap(170, 10, batt3, 30, 20, primary_color); //Battery icon
+ draw_scale();
+}
+
+/**********************************************************************/
